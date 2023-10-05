1613466745d597802c502e7278847cfb16ce95e4
JJMoraless <jjmorales.dev@gmail.com>
2023-10-05 07:22:05 -0500

    feat: :sparkles: configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    port: 5036,
+    host: "127.127.127.127"
+  }
 })
+
+// 192.168.129.72
