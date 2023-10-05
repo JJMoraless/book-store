2216cdffd9815c7661971081d1e34312709d48ef
JJMoraless <jjmorales.dev@gmail.com>
2023-10-05 05:34:33 -0500

    feat: :sparkles: añadido  page de libros

--- src/books/pages/MangaPage.jsx
+++ src/books/pages/MangaPage.jsx
@@ -1,24 +1,9 @@
-import { useEffect } from "react";
 import { BookList } from "../components/BookList";
-import { bookStoreApi } from "../../helpers/request";
 
 export const MangaPage = () => {
-  const getPorducts = async () => {
-    const products = bookStoreApi.get("products");
-    console.log(
-      "🚀 ~ file: MangaPage.jsx:7 ~ getPorducts ~ products:",
-      products
-    );
-  };
-
-  useEffect(() => {
-    getPorducts();
-  }, []);
-
   return (
     <>
-      <BookList publisher="DC Comics" />
-      <BookList publisher="Marvel Comics" />
+      <BookList publisher="🧙‍♂️ Free books" />
     </>
   );
 };
